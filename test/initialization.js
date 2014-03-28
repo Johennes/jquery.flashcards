9cb66807d423c07d8bc888b26f3d87f84cd3a202
Johannes Marbach <johannesmarbach@gmail.com>
2014-03-28 15:11:25 +0100

    Fixed head and food generator unit test

--- test/initialization.js
+++ test/initialization.js
@@ -14,10 +14,20 @@ test('initialization with defaults', function() {
 test('initialization with parameters', function() {
   var $fixture = $('#qunit-fixture');
   
+  var questionSide = "back";
+  var headGenerator = function() { return 'head' };
+  var footGenerator = function() { return 'foot' };
+  
   $fixture.flashcards({
-    questionSide: "back"
+    questionSide:  questionSide,
+    headGenerator: headGenerator,
+    footGenerator: footGenerator
   });
   
-  strictEqual($fixture.data('settings').questionSide, "back",
+  strictEqual($fixture.data('settings').questionSide, questionSide,
     'question side initialized with specified parameter');
+  deepEqual($fixture.data('settings').headGenerator, headGenerator,
+    'head generator initialized with specified parameter');
+  deepEqual($fixture.data('settings').footGenerator, footGenerator,
+    'foot generator initialized with specified parameter');
 });
